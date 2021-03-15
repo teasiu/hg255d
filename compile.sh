5f8b156434340b3190b31f6baa60c6c604ded84e
teasiu <teasiu@qq.com>
2021-03-15 11:00:13 +0800

    update

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -d tmp ];then
+	rm -rf tmp && rm .config .config.old
+fi
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 ./scripts/feeds install -a
@@ -17,11 +20,12 @@ cp -a diy/header.htm feeds/luci/themes/bootstrap/luasrc/view/themes/bootstrap
 cp -a diy/indexer.htm feeds/luci/themes/bootstrap/luasrc/view/themes/bootstrap
 cp -a diy/sysauth.htm feeds/luci/modules/base/luasrc/view
 cp -a diy/nfsd.exports feeds/packages/net/nfs-kernel-server/files
+cp -a diy/dnsmasq.conf package/network/services/dnsmasq/files/
 ./scripts/feeds install -a
-cp -a hg255d.config .config
-mkdir dl
-cp -a diy/download/* dl/
-wget -O dl/linux-firmware-2014-06-04-7f388b4885cf64d6b7833612052d20d4197af96f.tar.bz2 http://www.ecoo.top:8082/openwrtdl/linux-firmware-2014-06-04-7f388b4885cf64d6b7833612052d20d4197af96f.tar.bz2
-make defconfig
+#cp -a hg255d.config .config
+#mkdir dl
+#cp -a diy/download/* dl/
+#wget -O dl/linux-firmware-2014-06-04-7f388b4885cf64d6b7833612052d20d4197af96f.tar.bz2 http://www.ecoo.top:8082/openwrtdl/linux-firmware-2014-06-04-7f388b4885cf64d6b7833612052d20d4197af96f.tar.bz2
+#make defconfig
 #make -j5
 
